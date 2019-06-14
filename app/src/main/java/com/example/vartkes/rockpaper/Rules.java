3f35cead29869d2d073022382fc069a54d8ab665
wartkeskaraman <vartkess_123@hotmail.com>
2019-06-14 15:18:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vartkes/rockpaper/Rules.java
@@ -0,0 +1,13 @@
+package com.example.vartkes.rockpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
